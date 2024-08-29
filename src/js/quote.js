0c4823aac05ce88a50136e999f0eaf2b958a0566
Денис Уцеха <d.utsekha@reactor.com.ua>
2024-08-30 00:38:45 +0300

    feat: add loader

--- src/js/quote.js
+++ src/js/quote.js
@@ -1,41 +1,77 @@
 import { request } from './services/api-service';
 
+const BACKUP_QUOTE = {
+  quote:
+    'Unfortunately, there was an error on the server, but as they say: "Even in failure, wisdom finds its voice."',
+  author: 'Team "Your Enegry"',
+};
+
 (async () => {
-  const quoteDescNode = document.querySelector('.quote-desc');
-  const quoteAuthorNode = document.querySelector('.quote-author');
+  const quoteFromStorage = getQuoteFromStorage();
 
-  let quote =
-    'Unfortunately, there was an error on the server, but as they say: "Even in failure, wisdom finds its voice."';
-  let author = 'Team "Your Enegry"';
+  let quote = BACKUP_QUOTE.quote;
+  let author = BACKUP_QUOTE.author;
 
-  const quoteFromLocalStorage = JSON.parse(localStorage.getItem('quote'));
   if (
-    quoteFromLocalStorage &&
-    typeof quoteFromLocalStorage?.quote === 'string' &&
-    typeof quoteFromLocalStorage?.author === 'string' &&
-    typeof quoteFromLocalStorage?.date === 'string'
+    validateStorageQuote(quoteFromStorage) &&
+    isTodayDate(quoteFromStorage.date)
   ) {
-    console.log('LocalStorage');
-    quote = quoteFromLocalStorage.quote;
-    author = quoteFromLocalStorage.author;
+    quote = quoteFromStorage.quote;
+    author = quoteFromStorage.author;
   } else {
     try {
-      console.log('FETCH');
-      const data = await request('quote');
-
-      quote = data.quote;
-      author = data.author;
-
+      const response = await request('quote');
+      quote = response.quote;
+      author = response.author;
       localStorage.setItem(
         'quote',
-        JSON.stringify({
-          ...data,
-          date: new Date(),
-        })
+        JSON.stringify({ ...response, date: new Date() })
       );
-    } finally {
-      quoteDescNode.textContent = quote;
-      quoteAuthorNode.textContent = author;
+    } catch (error) {
+      console.log(error);
     }
   }
+
+  renderQuote(quote, author);
 })();
+
+function renderQuote(quote, author) {
+  const quoteNode = document.querySelector('.quote-desc');
+  const authorNode = document.querySelector('.quote-author');
+
+  quoteNode.textContent = quote;
+  authorNode.textContent = author;
+  quoteNode.classList.remove('skeleton');
+  authorNode.classList.remove('skeleton');
+}
+
+function getQuoteFromStorage() {
+  let quoteFromStorage;
+
+  try {
+    quoteFromStorage = JSON.parse(localStorage.getItem('quote'));
+  } catch (error) {
+    console.error(error);
+  }
+
+  return quoteFromStorage;
+}
+
+function validateStorageQuote(quote) {
+  return (
+    quote &&
+    typeof quote?.quote === 'string' &&
+    typeof quote?.author === 'string' &&
+    typeof quote?.date === 'string'
+  );
+}
+
+function isTodayDate(date) {
+  let dateFromStorage = new Date(date);
+  let today = new Date();
+
+  today.setHours(0, 0, 0, 0);
+  dateFromStorage.setHours(0, 0, 0, 0);
+
+  return dateFromStorage.getTime() === today.getTime();
+}
