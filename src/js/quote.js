a8a7679eb88f5ce210083546b438c0ad79c05545
Денис Уцеха <d.utsekha@reactor.com.ua>
2024-08-30 17:03:36 +0300

    IIFE

--- src/js/quote.js
+++ src/js/quote.js
@@ -1,85 +1,87 @@
 import { request } from './services/api-service';
 
-const BACKUP_QUOTE = {
-  text: 'Unfortunately, there was an error on the server, but as they say: "Even in failure, wisdom finds its voice."',
-  author: 'Team "Your Enegry"',
-};
+(async () => {
+  const BACKUP_QUOTE = {
+    text: 'Unfortunately, there was an error on the server, but as they say: "Even in failure, wisdom finds its voice."',
+    author: 'Team "Your Enegry"',
+  };
 
-// Main
-const quoteFromStorage = getQuoteFromStorage();
-const quote = {
-  text: BACKUP_QUOTE.text,
-  author: BACKUP_QUOTE.author,
-};
+  // Main
+  const quoteFromStorage = getQuoteFromStorage();
+  const quote = {
+    text: BACKUP_QUOTE.text,
+    author: BACKUP_QUOTE.author,
+  };
 
-if (
-  isQuoteFromStorageValid(quoteFromStorage) &&
-  isTodayDate(quoteFromStorage.date)
-) {
-  quote.text = quoteFromStorage.text;
-  quote.author = quoteFromStorage.author;
-} else {
-  try {
-    await fetchAndSaveQuote(quote);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-renderQuote(quote);
-
-// Utils
-async function fetchAndSaveQuote(quote) {
-  const response = await request('quote');
-  quote.text = response.quote;
-  quote.author = response.author;
-  localStorage.setItem(
-    'quote',
-    JSON.stringify({
-      text: response.quote,
-      author: response.author,
-      date: new Date(),
-    })
-  );
-}
-
-function getQuoteFromStorage() {
-  let quoteFromStorage;
-
-  try {
-    quoteFromStorage = JSON.parse(localStorage.getItem('quote'));
-  } catch (error) {
-    console.error(error);
+  if (
+    isQuoteFromStorageValid(quoteFromStorage) &&
+    isTodayDate(quoteFromStorage.date)
+  ) {
+    quote.text = quoteFromStorage.text;
+    quote.author = quoteFromStorage.author;
+  } else {
+    try {
+      await fetchAndSaveQuote(quote);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
-  return quoteFromStorage;
-}
+  renderQuote(quote);
 
-function renderQuote(quote) {
-  const quoteNode = document.querySelector('.quote-desc');
-  const authorNode = document.querySelector('.quote-author');
+  // Utils
+  async function fetchAndSaveQuote(quote) {
+    const response = await request('quote');
+    quote.text = response.quote;
+    quote.author = response.author;
+    localStorage.setItem(
+      'quote',
+      JSON.stringify({
+        text: response.quote,
+        author: response.author,
+        date: new Date(),
+      })
+    );
+  }
 
-  quoteNode.textContent = quote.text;
-  authorNode.textContent = quote.author;
-  quoteNode.classList.remove('skeleton');
-  authorNode.classList.remove('skeleton');
-}
+  function getQuoteFromStorage() {
+    let quoteFromStorage;
 
-function isQuoteFromStorageValid(quote) {
-  return (
-    quote &&
-    typeof quote?.text === 'string' &&
-    typeof quote?.author === 'string' &&
-    typeof quote?.date === 'string'
-  );
-}
+    try {
+      quoteFromStorage = JSON.parse(localStorage.getItem('quote'));
+    } catch (error) {
+      console.error(error);
+    }
 
-function isTodayDate(date) {
-  let dateFromStorage = new Date(date);
-  let today = new Date();
+    return quoteFromStorage;
+  }
 
-  today.setHours(0, 0, 0, 0);
-  dateFromStorage.setHours(0, 0, 0, 0);
+  function renderQuote(quote) {
+    const quoteNode = document.querySelector('.quote-desc');
+    const authorNode = document.querySelector('.quote-author');
 
-  return dateFromStorage.getTime() === today.getTime();
-}
+    quoteNode.textContent = quote.text;
+    authorNode.textContent = quote.author;
+    quoteNode.classList.remove('skeleton');
+    authorNode.classList.remove('skeleton');
+  }
+
+  function isQuoteFromStorageValid(quote) {
+    return (
+      quote &&
+      typeof quote?.text === 'string' &&
+      typeof quote?.author === 'string' &&
+      typeof quote?.date === 'string'
+    );
+  }
+
+  function isTodayDate(date) {
+    let dateFromStorage = new Date(date);
+    let today = new Date();
+
+    today.setHours(0, 0, 0, 0);
+    dateFromStorage.setHours(0, 0, 0, 0);
+
+    return dateFromStorage.getTime() === today.getTime();
+  }
+})();
