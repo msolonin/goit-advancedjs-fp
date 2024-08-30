53796cbc44c41c1aa3256e2f1c423911e91c7805
Iryna Kyryllova <keira.kirillova@gmail.com>
2024-08-30 11:22:06 +0200

    add desktop header

--- src/js/header.js
+++ src/js/header.js
@@ -1,8 +1,19 @@
-const openMenuBtn = document.querySelector('.js-open-menu');
-const closeMenuBtn = document.querySelector('.js-close-menu');
 const mobileMenu = document.querySelector('.js-mobile-menu');
 
+// Add active menu items
+const pathnamePage = window.location.pathname.split('/').pop();
+const currentPage = mobileMenu.querySelector(
+  `.nav-link[href='./${pathnamePage}']`
+);
+
+if (currentPage) {
+  currentPage.classList.add('active');
+}
+
 // Open/close mobile menu
+const openMenuBtn = document.querySelector('.js-open-menu');
+const closeMenuBtn = document.querySelector('.js-close-menu');
+
 openMenuBtn.addEventListener('click', function () {
   mobileMenu.classList.add('active');
 });
