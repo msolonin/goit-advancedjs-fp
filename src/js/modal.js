4ae25d31e3ab9217b382bf1bb72ba78da4164718
Karolina Nikolaienko <mandarinkanasolntse@gmail.com>
2024-09-01 23:53:58 +0200

    modal add rating html/css

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,5 +1,6 @@
 import { GetFavorites, AddToFavorites, RemoveFromFavorites } from "./utils/local-storage.js";
 import { request } from "./services/api-service.js";
+import iconsPath from "../img/icons.svg";
 
 const btnCloseModal = document.querySelector("[data-modal-close]");
 const modalWindow = document.querySelector(".modal"); // Modal without backdrop
@@ -28,12 +29,12 @@ async function LoadModalData(_id) {
     if (GetFavorites().indexOf(exerciseID) >= 0) {
         btnAddFavorites.classList.add("fav-added");
         document.querySelector(".btn-fav-text").textContent = "Remove from favorites";
-        document.querySelector(".icon-fav-btn-use").setAttribute("href", "./img/icons.svg#icon-trash");
+        document.querySelector(".icon-fav-btn-use").setAttribute("href", `${iconsPath}#icon-trash`);
     }
     else {
         btnAddFavorites.classList.remove("fav-added");
         document.querySelector(".btn-fav-text").textContent = "Add to favorites";
-        document.querySelector(".icon-fav-btn-use").setAttribute("href", "./img/icons.svg#icon-heart");
+        document.querySelector(".icon-fav-btn-use").setAttribute("href", `${iconsPath}#icon-heart`);
     }
 
     AddBtnFavListener();
@@ -96,12 +97,12 @@ function AddBtnFavListener() {
         if (btn.classList.contains("fav-added")) {
             AddToFavorites(exerciseID);
             document.querySelector(".btn-fav-text").textContent = "Remove from favorites";
-            document.querySelector(".icon-fav-btn-use").setAttribute("href", "./img/icons.svg#icon-trash");
+            document.querySelector(".icon-fav-btn-use").setAttribute("href", `${iconsPath}#icon-trash`);
         }
         else {
             RemoveFromFavorites(exerciseID);
             document.querySelector(".btn-fav-text").textContent = "Add to favorites";
-            document.querySelector(".icon-fav-btn-use").setAttribute("href", "./img/icons.svg#icon-heart");
+            document.querySelector(".icon-fav-btn-use").setAttribute("href", `${iconsPath}#icon-heart`);
         }
 
         event.stopImmediatePropagation();
@@ -133,20 +134,46 @@ btnCloseModal.addEventListener("click", () => {
 });
 
 modal.addEventListener("click", (event) => {
-    const rect = modalWindow.getBoundingClientRect();
-    // Define if cursor is in modal window
-    const isInWindow = (rect.top <= event.clientY && event.clientY <= rect.top + rect.height &&
-    rect.left <= event.clientX && event.clientX <= rect.left + rect.width);
-    if (!isInWindow) {
-        modal.classList.add("hidden");
-        OnClose();
+    let rect = "";
+    // If exercise modal window open
+    if (!modalWindow.classList.contains("hidden")) 
+        rect = modalWindow.getBoundingClientRect();
+    else if (!modalAddRatingWindow.classList.contains("hidden"))
+        rect = modalAddRatingWindow.getBoundingClientRect();
+
+    if (rect) {
+        // Define if cursor is in modal window
+        const isInWindow = (rect.top <= event.clientY && event.clientY <= rect.top + rect.height &&
+            rect.left <= event.clientX && event.clientX <= rect.left + rect.width);
+        if (!isInWindow) {
+            // If exercise modal opened
+            if (!modalWindow.classList.contains("hidden")) {
+                modal.classList.add("hidden");
+                OnClose();
+            }
+            // If Give a rating modal opened
+            else if (!modalAddRatingWindow.classList.contains("hidden") && !giveRatingIsPressed) {
+                modalAddRatingWindow.classList.add("hidden");
+                modalWindow.classList.remove("hidden");
+            }
+            giveRatingIsPressed = false;
+        }
     }
 });
 
 function escapeKeyDown(event) {
     if (event.key === "Escape") {
-        modal.classList.add("hidden");
-        OnClose();
+        // If exercise modal opened
+        if (!modalWindow.classList.contains("hidden")) {
+            modal.classList.add("hidden");
+            OnClose();
+        }
+        // If Give a rating modal opened
+        else if (!modalAddRatingWindow.classList.contains("hidden")) {
+            modalAddRatingWindow.classList.add("hidden");
+            modalWindow.classList.remove("hidden");
+            // OnClose();
+        }
     }
 }
 
@@ -162,4 +189,30 @@ function OnClose() {
     document.querySelector('.scroll-up-button').style.visibility = 'visible';
 }
 
+// Give a rating button
+const modalAddRatingWindow = document.querySelector(".add-rating-modal"); // Modal Give a rating
+const btnOpenRatingModal = document.querySelector("[data-add-rating-open]");
+let giveRatingIsPressed = false;
 
+btnOpenRatingModal.addEventListener("click", (event) => {
+    giveRatingIsPressed = true;
+    modalAddRatingWindow.classList.remove("hidden");
+    modalWindow.classList.add("hidden");
+    OnOpen();
+    event.stopImmediatePropagation();
+});
+
+// Give a rating modal
+const btnCloseRatingModal = document.querySelector("[data-add-rating-close]");
+
+// Rating modal close functions
+btnCloseRatingModal.addEventListener("click", (event) => {
+    modalAddRatingWindow.classList.add("hidden");
+    modalWindow.classList.remove("hidden");
+    // OnClose();
+    event.stopImmediatePropagation();
+});
+
+function LoadRatingModal() {
+    const radioBtns = document.querySelectorAll("add-rating-radio-btn");
+}
